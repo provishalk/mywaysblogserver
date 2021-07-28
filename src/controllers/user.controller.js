9fd4b71231681af1e23b2a22f2cb454057c6d985
provishalk <provishalk@gmail.com>
2021-07-28 22:57:39 +0530

    Form Validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,10 @@ module.exports.register = async (req, res) => {
     const email = req.body.email;
     const user = new User(req.body);
     if (await User.findOne({ email })) {
-        return res.status(404).send("User already exist");
+        return res.status(400).send({
+            error: true,
+            message: "Unauthorised Access, Please try with correct credentials",
+          });
     }
     else {
         user.save();
